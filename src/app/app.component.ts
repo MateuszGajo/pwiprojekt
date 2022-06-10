054a79144fe38c0f8794953753e09daeb786cc5a
ruslav <mateusz.gajo9@wp.pl>
2022-06-10 08:40:31 +0200

    last touches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor() {}
+  isHidden = false;
+
+  ngOnInit(): void {
+    const cookie = localStorage.getItem('cookie');
+    if (cookie) {
+      this.isHidden = true;
+    }
+  }
+
+  onClick() {
+    localStorage.setItem('cookie', 'true');
+    this.isHidden = true;
+  }
+
   title = 'pwiProjekt';
 }
